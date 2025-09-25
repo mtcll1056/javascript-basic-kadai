8478bddef497080d5925f933c765d0839c41cfba
mtcll1056 <ueno.tocollo@gmail.com>
2025-09-25 11:41:34 +0900

    Add files via upload

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,12 +1,12 @@
-// 今日の日付を取得
+// 今日の日付
 const today = new Date();
 
-// 年・月・日をそれぞれ取り出す
+// 年月日
 const year = today.getFullYear();
-const month = today.getMonth() + 1; // getMonthは0始まりなので+1
+const month = today.getMonth() + 1; 
 const day = today.getDate();
 
-// 出力フォーマット（YYYY-MM-DD）
+// 出力
 const formatted = `${year}年${String(month).padStart(2, '0')}月${String(day).padStart(2, '0')}日`;
 
 console.log(formatted);
